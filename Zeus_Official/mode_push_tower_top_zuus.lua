3834ba40974bb176eb7f47d335c50904ef725cc0
Hector Hernandez Morales <hectorhm1596@gmail.com>
2019-04-29 15:21:50 -0500

    Add files via upload
    sobreescribir

---
+++ Zeus_Official/mode_push_tower_top_zuus.lua
@@ -0,0 +1,14 @@
+function  OnStart()
+end
+
+function OnEnd()
+end
+-- dara un numero entre 0 y 1 que determina que tanto quieres hacer esta acion 
+function GetDesire()
+
+return 0;
+
+end
+
+function Think()
+end
